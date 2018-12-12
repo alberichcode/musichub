db2b4052aa896ad881ba076f05b82cb93064fae3
alberichcode <al.gp123@gmail.com>
2018-12-12 02:18:42 +0100

    añadidos shots aunque aun no se puedan ver en navegador debido a que no hay comments

--- musichub/db/schema.rb
+++ musichub/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_221145) do
+ActiveRecord::Schema.define(version: 2018_12_11_235815) do
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_221145) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_shot"
   end
 
   create_table "users", force: :cascade do |t|

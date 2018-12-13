5a76d07ce50caded13793018c3c58f30d7e3e44d
alberichcode <al.gp123@gmail.com>
2018-12-13 02:38:53 +0100

    shots añadidos junto con comentarios

--- musichub/db/schema.rb
+++ musichub/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_235815) do
+ActiveRecord::Schema.define(version: 2018_12_13_002027) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "shot_id"
+  end
 
   create_table "shots", force: :cascade do |t|
     t.string "title"

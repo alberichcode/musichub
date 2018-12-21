f2ca29ff2016c51dd81ce6861d45a2cf52f29671
alberichcode <al.gp123@gmail.com>
2018-12-21 01:50:08 +0100

    añadido un prototipo de lo que seria el acordeon del perfil personal

--- musichub/app/assets/javascripts/shots.js
+++ musichub/app/assets/javascripts/shots.js
@@ -102,4 +102,25 @@ document.addEventListener("turbolinks:load", function() {
 	Shots.shotHover();
 
 
+
+	var accItem = document.getElementsByClassName('accordionItem');
+	    var accHD = document.getElementsByClassName('accordionItemHeading');
+	    for (i = 0; i < accHD.length; i++) {
+	        accHD[i].addEventListener('click', toggleItem, false);
+	    }
+	    function toggleItem() {
+	        var itemClass = this.parentNode.className;
+	        for (i = 0; i < accItem.length; i++) {
+	            accItem[i].className = 'accordionItem close';
+	        }
+	        if (itemClass == 'accordionItem close') {
+	            this.parentNode.className = 'accordionItem open';
+	        }
+	    }
+
+
+
+
+
+
 });

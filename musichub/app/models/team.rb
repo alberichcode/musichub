ab90097259e685ca71d2d1d6d21f2bc0dbd8537e
alberichcode <al.gp123@gmail.com>
2018-12-26 04:37:31 +0100

    añadido el vuejs

--- musichub/app/models/team.rb
+++ musichub/app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ApplicationRecord
   has_many :users
   has_one :project
 
-  accepts_nested_attributes_for :user, allow_destroy: true # se asigna a : en este caso user
+  accepts_nested_attributes_for :users, allow_destroy: true # se asigna a : en este caso user
 end
